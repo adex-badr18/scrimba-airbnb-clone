535c8994514dc814a237ee4dbc3450919a74ac57
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-27 12:09:46 +0100

    Create & render card component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className='container'>
       <Navbar />
-      <Hero />
+
+      <main>
+        <Hero />
+        <Card />
+      </main>
     </div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import cardImage from '../assets/images/card-img.png';
+
+export default function Card() {
+    return (
+        <section className='card--container'>
+            <img src={cardImage} className='card--img' alt="Card" />
+            <span className="card--status">SOLD OUT</span>
+            <div className="card--rating-container">
+                <i className="fa-regular fa-star card--rating-icon"></i>
+                <span className="card--rating">5.0</span>
+                <span>(6)</span>
+                <i className="fa-regular fa-circle card--rating-dot"></i>
+                <span className="card--rating-country">USA</span>
+            </div>
+
+            <p className="card--description">
+                Life lessons with Katie Zaferes
+            </p>
+
+            <p className="card--price">
+                <span className="">From $136</span> / person
+            </p>
+        </section>
+    )
+}

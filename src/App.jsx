ca416cb696109df98ddf369434fbcd7af8a70549
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-27 18:24:37 +0100

    Pass props to card component

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,27 @@ function App() {
 
       <main>
         <Hero />
-        <Card />
+        <Card 
+          img='card-img.png'
+          rating='5.0'
+          reviewCount={6}
+          country='Nigeria'
+          title='Life Lessons with Katie Zaferes'
+          price='136'
+        />
       </main>
     </div>
   )
 }
 
 export default App
+
+/*
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,25 @@
 import cardImage from '../assets/images/card-img.png';
 
-export default function Card() {
+export default function Card(props) {
+    const {img, rating, reviewCount, country, title, price} = props;
     return (
         <div className='card--container'>
-            <img src={cardImage} className='card--img' alt="Card" />
+            <img src={`/${img}`} className='card--img' alt="Card" />
             <span className="card--status">SOLD OUT</span>
             <div className="card--rating-container">
                 <i className="fa-solid fa-star card--rating-icon"></i>
-                <span className="card--rating">5.0</span>
-                <span>(6)</span>
+                <span className="card--rating">{rating}</span>
+                <span>({reviewCount})</span>
                 <i className="fa-solid fa-circle card--rating-dot"></i>
-                <span className="card--rating-country">USA</span>
+                <span className="card--rating-country">{country}</span>
             </div>
 
             <p className="card--description">
-                Life lessons with Katie Zaferes
+                {title}
             </p>
 
             <p className="card--price">
-                <span className="">From $136</span> / person
+                <span className="">From ${price}</span> / person
             </p>
         </div>
     )

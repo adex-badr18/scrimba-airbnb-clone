26ca048861dd8bc16888284d254cc29c3ac4a257
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-28 10:25:04 +0100

    Pass objects as props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,33 @@
 // import cardImage from '../assets/images/card-img.png';
 
 export default function Card(props) {
-    const {img, rating, reviewCount, location, title, price} = props;
+    const obj = props.item;
 
     let badgeText;
-    if (props.openSpots === 0) {
+    if (obj.openSpots === 0) {
         badgeText = 'SOLD OUT';
-    } else if (props.location === 'Online') {
+    } else if (obj.location === 'Online') {
         badgeText = 'ONLINE';
     }
 
     return (
         <div className='card--container'>
-            <img src={`/images/${img}`} className='card--img' alt="Card" />
+            <img src={`/images/${obj.coverImg}`} className='card--img' alt="Card" />
             {badgeText && <span className="card--status">{badgeText}</span>}
             <div className="card--rating-container">
                 <i className="fa-solid fa-star card--rating-icon"></i>
-                <span className="card--rating">{rating}</span>
-                <span>({reviewCount})</span>
+                <span className="card--rating">{obj.stats.rating}</span>
+                <span>({obj.stats.reviewCount})</span>
                 <i className="fa-solid fa-circle card--rating-dot"></i>
-                <span className="card--rating-country">{location}</span>
+                <span className="card--rating-country">{obj.location}</span>
             </div>
 
             <p className="card--description">
-                {title}
+                {obj.title}
             </p>
 
             <p className="card--price">
-                <span className="">From ${price}</span> / person
+                <span className="">From ${obj.price}</span> / person
             </p>
         </div>
     )
